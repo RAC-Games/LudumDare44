b8ee2f4feb085588c1ea658013d0ae2d65f8773b
step4 <c.rausch020@gmail.com>
2019-04-27 14:13:45 +0200

    new talk scene

---
+++ Assets/startBuyScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startBuyScreen : MonoBehaviour
+{
+    public Image image;
+    public Text text;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        panel.SetActive(true);
+        text.text = "hello";
+    }
+}
