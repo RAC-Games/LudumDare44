778bdb8eb073fad91d1b70fafbe36da5007343ac
Luisa <luisa.kalkert@gmx.de>
2019-04-27 23:05:58 +0200

    Added HeartPlaceHolder and DeathEvent

---
+++ Assets/HealthDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    public Health playerHealth;
+    public Image heart;
+    // Update is called once per frame
+
+    void Update()
+    {
+        for(int i = 1;i < playerHealth.health; i++)
+        {
+            Instantiate(heart, transform.position + i*new Vector3(-10,0,0), transform.rotation, transform);
+        }
+    }
+}
---
+++ Assets/persistent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class persistent : MonoBehaviour
+{
+    public string cameFrom;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
