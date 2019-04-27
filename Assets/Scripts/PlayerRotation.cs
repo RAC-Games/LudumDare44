6d8fbae58b92906f269ec48cc085583b1993a450
L´s <to.buy@t-onlilne.de>
2019-04-27 10:21:06 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public GameObject mover;
+    public float moverSpeed;
+    public float moverDist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Mouse X");
+        var y = Input.GetAxis("Mouse Y");
+
+
+        var relativeMovement = new Vector3(x, 0, y);
+
+        var newPos = mover.transform.position + relativeMovement * moverSpeed;
+        if (Vector3.Distance(newPos, transform.position) < moverDist)
+        {
+            mover.transform.position = newPos;
+        }
+        else
+        {
+            mover.transform.position = (newPos - transform.position).normalized * moverDist + transform.position;
+        }
+        var lookPos = new Vector3(mover.transform.position.x,transform.position.y, mover.transform.position.z);
+        transform.LookAt(lookPos);
+    }
+}
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent agent;
+
+    public float stopDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position,player.transform.position)> stopDistance)
+        {
+            agent.isStopped = false;
+            agent.destination = player.transform.position;
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
+    }
+}
