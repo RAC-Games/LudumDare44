988dc627fc6618b54959ed620ce7a5bbd16dcfa4
step4 <c.rausch020@gmail.com>
2019-04-27 10:55:25 +0200

    shooting works

---
+++ Assets/AttackPrefab/PlayerAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(projectile))]
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject attack1;
+    public GameObject attack2;
+    public Vector3 mousePosition;
+    public Transform startPosition;
+    projectile projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = GetComponent<projectile>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            projectile.shoot(startPosition.position, transform.forward, attack1);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            projectile.shoot(startPosition.position, transform.forward, attack2);
+        }
+    }
+}
---
+++ Assets/AttackPrefab/projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    
+    Rigidbody rb;
+
+    public void shoot(Vector3 startPosition, Vector3 targetPosition,  GameObject gameObject)
+    {
+        GameObject instance = Instantiate(gameObject, startPosition, new Quaternion(0,0,0,0));
+        rb = instance.GetComponent<Rigidbody>();
+        rb.AddForce(50*(targetPosition),ForceMode.Impulse);
+        Destroy(instance, 1);
+    }
+}
