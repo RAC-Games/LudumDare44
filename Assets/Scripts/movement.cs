46ce5df239f23bf502193ed29885179b803e3d97
step4 <c.rausch020@gmail.com>
2019-04-27 06:37:44 +0200

    movement script

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject mouseHit;
+    Rigidbody rb;
+
+    Vector3 rotation = new Vector3(0, 0, 0);
+    Vector2 oldInput;
+    float angle;
+
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 oldInput = new Vector2(0, 0);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var direction = new Vector3(horizontal, 0, vertical);
+        rb.MovePosition(transform.position + direction * Time.deltaTime);
+
+
+
+        var x = Input.GetAxis("Mouse X");
+        var y = Input.GetAxis("Mouse Y");
+        var delta = oldInput - new Vector2(x, y);
+        
+        oldInput = new Vector2(x, y);
+
+        delta = delta.normalized;
+        print(delta);
+        angle = Vector2.Angle(transform.forward, delta);
+        
+        transform.rotation=Quaternion.Euler(transform.rotation.eulerAngles.x,angle, transform.rotation.eulerAngles.z);
+
+    }
+}
