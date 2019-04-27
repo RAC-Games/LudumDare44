b42a0b922d6f071ff4d30df1a01dbaa4a7a609cd
Luisa <luisa.kalkert@gmx.de>
2019-04-27 15:08:22 +0200

    AddedPlayerHealthAndCollisionTags

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public Health healthSO;
+
+    private void Start()
+    {
+        healthSO.health = 10;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            healthSO.decreaseHealth(1);
+            print(healthSO.health);
+        }
+    }
+}
