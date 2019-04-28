e95d266cd8bbb326a9d470b363ffa250802d49fb
step4 <c.rausch020@gmail.com>
2019-04-28 11:38:51 +0200

    level 1 doors ready

---
+++ Assets/ScriptableObjects/progressSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "progress", menuName = "scriptableObjects/progress", order = 1)]
+public class progressSO : ScriptableObject
+{
+    public bool level1;
+    public bool level2;
+    public bool level3;
+    public bool level4;
+}
