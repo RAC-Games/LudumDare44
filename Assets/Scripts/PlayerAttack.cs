266023f9044ec3f3b52d3c8fcb8ef036beff9f9b
Luisa <luisa.kalkert@gmx.de>
2019-04-28 18:01:26 +0200

    added Attack animation to Player

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -9,7 +9,7 @@ public class PlayerAttack : MonoBehaviour
     public GameObject attack1;
     public GameObject attack2;
     public Transform startPosition;
-    Animator anim;
+    public Animator anim;
 
     projectile projectile;
     public float elapsed = 0f;
@@ -21,7 +21,6 @@ public class PlayerAttack : MonoBehaviour
     void Start()
     {
         projectile = GetComponent<projectile>();
-        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,13 +33,14 @@ public class PlayerAttack : MonoBehaviour
             if (Input.GetMouseButton(0))
             {
                 elapsed = 0;
-                //animation1
+                anim.SetTrigger("attack1");
                 projectile.playerShoot(startPosition.position, transform.forward, attack1, attack1Speed);
             }
 
             if (Input.GetMouseButton(1))
             {
                 //animation2
+                anim.SetTrigger("attack2");
                 elapsed = 0;
                 projectile.playerShoot(startPosition.position, transform.forward, attack2, attack2Speed);
             }
