e3a95ca84c475054138ba233160de3b95da4473d
step4 <c.rausch020@gmail.com>
2019-04-28 15:58:32 +0200

    progress kinda working

---
+++ Assets/setInitDoorProgress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setInitDoorProgress : MonoBehaviour
+{
+    public progressSO progress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        progress.door1 = true;
+        progress.door2 = false;
+        progress.door3 = false;
+        progress.door4 = false;
+        progress.doorHub = true;
+        progress.doorBoss = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
