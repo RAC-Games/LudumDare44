550c44c7247164e469a74d2d84350eb6324550a6
Luisa <luisa.kalkert@gmx.de>
2019-04-28 13:14:18 +0200

    Added a Canvas Manger

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject healthCanvas;
+
+    private void OnEnable()
+    {
+        if(SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            healthCanvas.SetActive(true);
+            healthCanvas.GetComponent<HeartUI>().UpdateHearts();
+            
+        }
+    }
+
+}
