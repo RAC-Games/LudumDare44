fffbd928f52e0d281d30fac6c283ddacba87be93
Luisa <luisa.kalkert@gmx.de>
2019-04-29 19:27:24 +0200

    started hubreturning on death

---
+++ Assets/returnToHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class returnToHub : MonoBehaviour
+{
+    public void teleport()
+    {
+        SceneManager.LoadScene("Hub_1");
+    }
+}
+s
